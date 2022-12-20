ced5cd94ca6949fd33c632ba1efad11d1f35340e
AleksanderBak <43186488+AleksanderBak@users.noreply.github.com>
2022-12-20 20:04:34 +0100

    Update WhatShouldIEat.java

--- WhatShouldIEat_SnackEdition/src/main/java/com/sample/WhatShouldIEat.java
+++ WhatShouldIEat_SnackEdition/src/main/java/com/sample/WhatShouldIEat.java
@@ -1,39 +1,6 @@
 
 package com.sample;
 
-import java.util.ArrayList;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
-
-import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JRadioButton;
-import javax.swing.JList;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.ListSelectionModel;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableColumnModel;
-
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
